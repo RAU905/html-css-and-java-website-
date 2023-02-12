4c07bb247e600db84d3fc801dcfa04d13fe3d5ec
Raushan kumar <111661802+RAU905@users.noreply.github.com>
2023-02-12 20:13:25 +0530

    Add files via upload
    this is a rough portfolio website made for traial for hosting it 
    later it will get add inn more features

---
+++ win.js
@@ -0,0 +1,51 @@
+// const candrive = (age) => {
+//   return age >= 18 ? true : false;
+// };
+
+// if (age >= 18) {
+//   alert("yes you can drive");
+// } else {
+//   alert("no you cannot drive");
+// }
+
+// age = prompt("enter a number");
+// age = Number.parseInt(age);
+
+// candrive = (age) => {
+//   return age >= 18 ? true : false;
+// };
+
+// if (candrive(age)) {
+//   alert("yes you can drive");
+// } else {
+//   alert("no let you cannot drive");
+// // }
+
+// const alphabet = "swg";
+
+// const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
+// // console.log(randomCharacter);
+// let n = prompt("enter s,w or g");
+// n = Number.parseInt(n);
+// const k = randomCharacter;
+// let score = 0;
+// if (k == n) {
+//   alert("tie");
+//   console.confirm(" do u want to play agin");
+// } else if (k == s && n == w) {
+//   alert("you loose");
+// } else if (k == s && n == g) {
+//   alert("you won");
+//   score++;
+// } else if (k == w && n == g) {
+//   alert("you loose");
+// } else if (k == w && n == s) {
+//   alert("you won");
+//   score++;
+// } else if (k == g && n == w) {
+//   alert("you loose");
+// } else if (k == g && n == s) {
+//   alert("you loose");
+// }
+
+// console.log(score);
